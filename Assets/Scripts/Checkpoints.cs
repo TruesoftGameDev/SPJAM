fd792cbf7f71057e598af609686c328b2fb10fec
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2013-08-31 14:39:40 -0300

    Corrigindo erros

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -5,10 +5,24 @@ public class Checkpoints : MonoBehaviour {
 	
 	public GameObject[] checkpoints; 
 	public int[] perspectivas;
-	
+	public int atual;
 	
 	void Start () {
-	
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		AttributesController controller = player.GetComponent<AttributesController>();
+		
+		if(PlayerPrefs.HasKey("Checkpoint"))
+		{
+			atual = PlayerPrefs.GetInt("Checkpoint");
+			controller.orbs = PlayerPrefs.GetInt("Orbs");
+			controller.vidas = PlayerPrefs.GetInt("Vidas");
+			controller.continues = PlayerPrefs.GetInt("Continues");
+			player.transform.position = checkpoints[atual].transform.position;
+		}	
+		else
+			atual = 0;
+		
+		
 	}
 	
 	
