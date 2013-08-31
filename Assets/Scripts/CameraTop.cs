5faf5d8532fcfbb113dd78fe164b5514af6d861d
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2013-08-31 10:03:57 -0300

    Segunda camera

---
+++ Assets/Scripts/CameraTop.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTop : MonoBehaviour {
+	
+	void Update () {
+		transform.position = new Vector3(0,transform.position.y, transform.position.z);	
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,25 +6,24 @@ public class PlayerController : MonoBehaviour {
 	public float velocidade = 3.0f;
 	public float pulo = 8.0f;
 	public float gravidade = 20.0f;
+	public float velocidadeLateral = 0.5f;
 	private Vector3 movimento;
 	public int perspectiva = 1;
 	public GameObject cameraSideRun;
 	public GameObject cameraTopRun;
 	
 	void Start () {
-		if(perspectiva == 1)
-		{
-			toSideCamera();
+		switch(perspectiva){
+			case 1:
+				toSideCamera();
+				break;
+			case 2:
+				toTopCamera();
+				break;
+			default:
+				Debug.Log ("Erro cameras!");
+				break;
 		}
-		else if(perspectiva == 2)
-		{
-			toTopCamera();
-		}
-		else
-		{
-			Debug.Log ("Erro camera");	
-		}
-		
 		
 	}
 	
@@ -32,37 +31,80 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		
 		
-		
-		//Garante que sempre fique na posiçao 0 do eixo X;
-		transform.position = new Vector3(0,transform.position.y, transform.position.z);	
-		
 		CharacterController characterController = GetComponent<CharacterController>();
-		
-		if(characterController.isGrounded)
+		//Movimenta o personagem para frente
+		movimento = Vector3.forward;
+		movimento = transform.TransformDirection(movimento);
+		movimento *= velocidade;
+		switch(perspectiva)
 		{
-			movimento = Vector3.back;
-			movimento = transform.TransformDirection(movimento);
-			movimento *= velocidade;
-			if(Input.GetKey(KeyCode.UpArrow))
-			{
-                movimento.y = pulo;
-			}
+			case 1:
+				//Garante que sempre fique na posiçao 0 do eixo X;
+				transform.position = new Vector3(0,transform.position.y, transform.position.z);	
+				
+				if(characterController.isGrounded)
+				{
+					
+					if(Input.GetKey(KeyCode.UpArrow))
+					{
+		                movimento.y = pulo;
+					}
+				}
+				break;
+			case 2:
+				
+				if(Input.GetKey(KeyCode.LeftArrow))
+				{
+					movimento += Vector3.left*velocidadeLateral;
+				}
+				if(Input.GetKey(KeyCode.RightArrow))
+				{
+					movimento += Vector3.right*velocidadeLateral;
+				}
+				
+				break;
 			
+			default:
+				Debug.Log ("Erro, perpectiva inexistente!");
+				break;
 		}
 		movimento.y -= gravidade*Time.deltaTime;
 		characterController.Move(movimento);
 	}
-	
-	
+	void trocaPerspectiva(int codPerspectiva)
+	{
+		switch(codPerspectiva)
+		{
+			case 1:
+				toSideCamera();
+				this.perspectiva = 1;
+				break;
+			case 2:
+				toTopCamera();
+				this.perspectiva = 2;
+				break;
+			default:
+				
+				break;
+		}
+	}
 	
 	//Teste de colisoes de morte, troca de camera
 	void OnControllerColliderHit(ControllerColliderHit colisor)
 	{
 		
-		if(colisor.collider.gameObject.tag == "DeathCollider")
+		switch(colisor.collider.gameObject.tag)
 		{
-			Debug.Log ("Entrou");
-			SendMessage("Dead");
+			case "DeathCollider":
+				SendMessage("Dead");
+				break;
+			case "Perspectiva1":
+				trocaPerspectiva(1);
+				break;
+			case "Perspectiva2":
+				trocaPerspectiva(2);
+				colisor.collider.enabled = false;
+				break;
 		}
 	}
 	private void toSideCamera(){
