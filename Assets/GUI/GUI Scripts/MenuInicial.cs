ae43a0b36840f51914654158b814bf366f91e99c
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2013-08-31 16:42:56 -0300

    Menu inicial

---
+++ Assets/GUI/GUI Scripts/MenuInicial.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Itens
+{
+	NovoJogo,
+	Continuar,
+	Creditos,
+	Sair
+}
+
+public class MenuInicial : MonoBehaviour {
+	
+	public GUITexture novoJogo;
+	public GUITexture continuar;
+	public GUITexture creditos;
+	public GUITexture sair;
+	public Texture2D[] padrao;
+	public Texture2D[] selecionados;
+	
+	private bool mudou = false;
+
+	Itens itens = Itens.NovoJogo;
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			itens--;
+			mudou = true;
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			itens++;
+			mudou = true;
+		}
+		if(mudou){
+			if((int)itens<0)
+				itens = Itens.Sair;
+			if((int)itens>3)
+				itens= Itens.NovoJogo;
+			switch(itens)
+			{
+				case Itens.NovoJogo:
+					novoJogo.texture = selecionados[0];
+					continuar.texture = padrao[1];
+					creditos.texture = padrao[2];
+					sair.texture = padrao[3];
+					break;
+				case Itens.Continuar:
+					novoJogo.texture = padrao[0];
+					continuar.texture = selecionados[1];
+					creditos.texture = padrao[2];
+					sair.texture = padrao[3];
+					break;
+				case Itens.Creditos:
+					novoJogo.texture = padrao[0];
+					continuar.texture = padrao[1];
+					creditos.texture = selecionados[2];
+					sair.texture = padrao[3];
+					break;
+				case Itens.Sair:
+					novoJogo.texture = padrao[0];
+					continuar.texture = padrao[1];
+					creditos.texture = padrao[2];
+					sair.texture = selecionados[3];
+					break;			
+			}
+			mudou = false;
+		}
+	}
+}
+
