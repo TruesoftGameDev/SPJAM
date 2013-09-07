5f299d14e321e71d4e90152f21b1c579b10997c6
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2013-09-07 00:14:32 -0300

    Velocidade e gameover

--- Assets/Scripts/AttributesController.cs
+++ Assets/Scripts/AttributesController.cs
@@ -87,6 +87,6 @@ public class AttributesController : MonoBehaviour {
 			Application.LoadLevel("Continue");
 		}
 		else
-			Application.LoadLevel(nivelAtual);
+			Application.LoadLevel("Gameover");
 	}
 }
