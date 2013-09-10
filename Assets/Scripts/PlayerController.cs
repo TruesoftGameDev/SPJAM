d58745fd04a1bd374c26696783bcabbac35f7a85
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2013-09-10 14:41:12 -0300

    Comentários  Controlador Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,35 +3,49 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
-	
+	//Velocidade
 	public float velocidade = 3.0f;
+	//Velocidade lateral (Perspectiva 2)
+	public float velocidadeLateral = 0.5f;
+	
+	//Pulo
 	public bool canJump = true;
 	public bool jumping = false;
 	public bool doubleJump = false;
 	public float pulo = 8.0f;
+	//Gravidade
 	public float gravidade = 20.0f;
-	public float velocidadeLateral = 0.5f;
+	
+	//Rasteira
+	/*
 	public bool rasteira = false;
 	public float tempoRasteira = 1.0f;
 	public  float tmpRasteira;
+	*/
+	//Pause
 	public bool pause = false;
 	
+	//Proximo movimento
 	private Vector3 movimento;
+	//Cameras
 	public int perspectiva = 1;
 	public GameObject cameraSideRun;
 	public GameObject cameraTopRun;
 	
+	//Som de pulo
 	public AudioSource jumpSound;
 	
 	//Touch
-	public bool touching = false;
-	public float delayJump = 0.1f;
+	private bool touching = false;
+	private float delayJump = 0.1f;
 	
 	void Start () {
 		
+		//Define velocidade de animaçao
 		animation["Run"].speed = 3.0f;
 		animation["Sliding"].speed = 3.0f;
 		
+		//Atualiza perspectiva
 		switch(perspectiva){
 			case 1:
 				toSideCamera();
@@ -43,19 +57,21 @@ public class PlayerController : MonoBehaviour {
 				Debug.Log ("Erro cameras!");
 				break;
 		}
-		tmpRasteira = tempoRasteira;
+		//tmpRasteira = tempoRasteira;
 		
 	}
 	
 	
 	void Update () {
 		
+		//Gerencia movimentaçao
 		if(!pause){		
 			CharacterController characterController = GetComponent<CharacterController>();
 			//Movimenta o personagem para frente
 			movimento = Vector3.forward;
 			movimento = transform.TransformDirection(movimento);
 			movimento *= velocidade;
+			//Atualiza movimentaçao e checa controles de acordo com a perspectiva
 			switch(perspectiva)
 			{
 				case 1:
@@ -69,10 +85,13 @@ public class PlayerController : MonoBehaviour {
 					Debug.Log ("Erro, perpectiva inexistente!");
 					break;
 			}
+			//Aplica gravidade
 			movimento.y -= gravidade*Time.deltaTime;
+			//Aplica movimentaçao
 			characterController.Move(movimento);
 			}
 	}
+	//Troca perspectiva
 	public void trocaPerspectiva(int codPerspectiva)
 	{
 		switch(codPerspectiva)
@@ -94,7 +113,7 @@ public class PlayerController : MonoBehaviour {
 	{
 		//Garante que sempre fique na posiçao 0 do eixo X;
 		transform.position = new Vector3(0,transform.position.y, transform.position.z);
-		//Touch
+		//Chega toques
 		if(Input.touchCount > 0)
 		{
 			touching = true;	
@@ -104,10 +123,11 @@ public class PlayerController : MonoBehaviour {
 		{
 			touching = false;	
 		}
-		
+		//Delay do pulo
 		if(jumping)
-			delayJump -= Time.deltaTime;
+			delayJump -= Time.deltaTimse;
 		
+		//Se estiver no chao, passa para animaçao de corrida
 		if(characterController.isGrounded)
 		{
 			canJump = true;
@@ -148,10 +168,14 @@ public class PlayerController : MonoBehaviour {
 			}
 			*/
 		}
+		
+		//Roda animaçao de queda 
 		else if(!jumping)
 		{
 			animation.Play("DoubleJump");
 		}
+		
+		//Gerencia controles de pulo
 		if((Input.GetKeyDown(KeyCode.UpArrow) || touching) && (canJump) )
 		{
             
